6befb46462b536f6edddeb163a091039fca419f3
ikkong.maipu <ikkong@qq.com>
2016-04-19 13:29:09 +0800

    增加部分方法

--- sunnylibrary/src/main/java/com/ikkong/sunnylibrary/utils/StringUtil.java
+++ sunnylibrary/src/main/java/com/ikkong/sunnylibrary/utils/StringUtil.java
@@ -1,9 +1,32 @@
 package com.ikkong.sunnylibrary.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * Created by ikkong on 2015/12/18.
  */
 public class StringUtil {
+    private final static Pattern emailer = Pattern
+            .compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+
+    /**
+     * 判断字符是否中文
+     * @param c char
+     * @return bool
+     */
+    public static boolean isChinese(char c) {
+        boolean result = false;
+        if (c >= 19968 && c <= 171941) {// 汉字范围 \u4e00-\u9fa5 (中文)
+            result = true;
+        }
+        return result;
+    }
 
     /**
      * 去除 str 尾部 的 t
@@ -43,5 +66,130 @@ public class StringUtil {
         String rel = trimEnd(str,t);
         rel = trimStart(rel,t);
         return rel;
+    }
+
+    /**
+     * 判断是不是一个合法的电子邮件地址
+     *
+     * @param email 字符
+     * @return bool
+     */
+    public static boolean isEmail(String email) {
+        if (email == null || email.trim().length() == 0)
+            return false;
+        return emailer.matcher(email).matches();
+    }
+
+    /**
+     * 字符串转整数
+     *
+     * @param str 字符
+     * @param defValue 默认值
+     * @return
+     */
+    public static int toInt(String str, int defValue) {
+        try {
+            return Integer.parseInt(str);
+        } catch (Exception e) {
+        }
+        return defValue;
+    }
+
+    /**
+     * 对象转整数
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static int toInt(Object obj) {
+        if (obj == null)
+            return 0;
+        return toInt(obj.toString(), 0);
+    }
+
+    /**
+     * 对象转整数
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static long toLong(String obj) {
+        try {
+            return Long.parseLong(obj);
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    /**
+     * 字符串转布尔值
+     *
+     * @param b
+     * @return 转换异常返回 false
+     */
+    public static boolean toBool(String b) {
+        try {
+            return Boolean.parseBoolean(b);
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    /**
+     * 将一个InputStream流转换成字符串
+     *
+     * @param is
+     * @return
+     */
+    public static String toConvertString(InputStream is) {
+        StringBuffer res = new StringBuffer();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader read = new BufferedReader(isr);
+        try {
+            String line;
+            line = read.readLine();
+            while (line != null) {
+                res.append(line);
+                line = read.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != isr) {
+                    isr.close();
+                    isr.close();
+                }
+                if (null != read) {
+                    read.close();
+                    read = null;
+                }
+                if (null != is) {
+                    is.close();
+                    is = null;
+                }
+            } catch (IOException e) {
+            }
+        }
+        return res.toString();
+    }
+
+    /**
+     * The string "boo:and:foo", for example, yields the following results with these expressions: 
+     Regex Result 
+     : { "boo", "and", "foo" }
+     o { "b", ":and:f" }
+     * @param str 要分割的字符串
+     * @param regex 分割参数
+     * @return
+     */
+    public static String[] splitNoEmpty(String str,String regex){
+        String A[] = str.split(regex);
+        List<String> list = new ArrayList<String>();
+        for (String a : A) {
+            if(!a.equals(""))
+                list.add(a);
+        }
+        return list.toArray(new String[0]);
     }
 }
