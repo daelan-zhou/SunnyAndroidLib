58b8e98ea5af933848bc7f2986097fae754eafa0
ikkong.maipu <ikkong@qq.com>
2016-06-08 16:02:14 +0800

    修复 文件名含%无法显示的问题

--- sunnyimagepreview/src/main/java/com/ikkong/sunnyimagepreview/loader/GlideImageLoader.java
+++ sunnyimagepreview/src/main/java/com/ikkong/sunnyimagepreview/loader/GlideImageLoader.java
@@ -1,19 +1,22 @@
 package com.ikkong.sunnyimagepreview.loader;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.ikkong.sunnyimagepreview.R;
 
+import java.io.File;
+
 /**
  * ================================================
- * 作    者：jeasonlzy（廖子尧 Github地址：https://github.com/jeasonlzy0216，ikkong
+ * 作    者：jeasonlzy，ikkong
  * 版    本：1.0
  * 创建日期：2016/5/19
  * 描    述：
- * 修订历史：保留图片预览，去除其他代码，更新display 方法
+ * 修订历史：保留图片预览，去除其他代码，更新display 方法  修复bug
  * ================================================
  */
 public class GlideImageLoader implements ImageLoader {
@@ -34,7 +37,7 @@ public class GlideImageLoader implements ImageLoader {
     @Override
     public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
         Glide.with(activity)                             //配置上下文
-                .load(path.contains("http://")?path:("file://"+path)) //设置图片路径
+                .load((path.contains("http://")||path.contains("https://"))?path: Uri.fromFile(new File(path)))//设置图片路径
                 .error(R.mipmap.default_image)           //设置错误图片
                 .placeholder(R.mipmap.default_image)     //设置占位图片
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存全尺寸
